e0b2bcad73144e51e30ce8f20053140e094203bf
ridwanmalik <skredonemalik@gmail.com>
2024-04-18 01:35:03 +0600

    Add viewport meta tag to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Open In WhatsApp",
   description: "Open WhatsApp chat without saving the number",
+  viewport:
+    "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=no",
 }
 
 export default function RootLayout({
